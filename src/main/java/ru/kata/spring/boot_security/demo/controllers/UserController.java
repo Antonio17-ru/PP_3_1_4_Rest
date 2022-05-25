1e44d7deb6ace512c3c5b656d2939cd645ec8524
Antonio17-ru <AntonDemeshkin1@yandex.ru>
2022-05-26 01:05:23 +0300

    Main fix commit

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -1,8 +1,6 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +10,6 @@ import ru.kata.spring.boot_security.demo.model.User;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
-    private UserDetailsService userDetailsService;
-    @Autowired
-    public UserController(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
-
     @GetMapping()
     public String index(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("user", user);
--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -36,4 +36,8 @@ public class Role implements GrantedAuthority {
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return name.replace("ROLE_", "");
+    }
 }
